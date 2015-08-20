9b7ff3345fdc4f0ee40c00496d8425ea0c9291cb
M0J01 <moodymusicman@gmail.com>
2015-08-20 17:18:37 -0400

    First Commit after Forking
    Change made to Jobs_Read.py, added a comment to the code.

--- jobs_read.py
+++ jobs_read.py
@@ -7,5 +7,6 @@ symbolslist = symbolfile.read()
 writingfile = open("jorbs.txt","w")
 writingfile.write(symbolslist)
 
-
 print symbolslist.split("\n")
+
+#Second Change made to file group since Git repository Snipets was formed.
